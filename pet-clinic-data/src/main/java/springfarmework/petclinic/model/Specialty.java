95be72928cd351d62de454f65775cecacfe4ce17
Michael <mmesropian@gmail.com>
2023-03-09 13:08:29 +0200

    add Specialty property

--- pet-clinic-data/src/main/java/springfarmework/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/springfarmework/petclinic/model/Specialty.java
@@ -1,4 +1,13 @@
 package springfarmework.petclinic.model;
 
 public class Specialty extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
