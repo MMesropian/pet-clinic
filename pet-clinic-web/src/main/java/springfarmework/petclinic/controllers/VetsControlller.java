b79c6d19d328905723feb600dbba84707e6c496e
Michael <mmesropian@gmail.com>
2023-03-10 15:08:12 +0200

    fixed broken links. closes #45

--- pet-clinic-web/src/main/java/springfarmework/petclinic/controllers/VetsControlller.java
+++ pet-clinic-web/src/main/java/springfarmework/petclinic/controllers/VetsControlller.java
@@ -13,7 +13,7 @@ public class VetsControlller {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
