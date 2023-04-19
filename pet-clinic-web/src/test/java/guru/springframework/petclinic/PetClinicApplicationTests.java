aa2b0efb831cf38714036568ff82ec3ea532747c
Michael <mmesropian@gmail.com>
2023-04-19 20:59:46 +0300

    added tests closes #49

--- pet-clinic-web/src/test/java/guru/springframework/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/PetClinicApplicationTests.java
@@ -1,9 +1,14 @@
 package guru.springframework.petclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+
+@SpringBootTest(classes = springfarmework.petclinic.PetClinicApplication.class)
+@ExtendWith(SpringExtension.class)
 class PetClinicApplicationTests {
 
     @Test
